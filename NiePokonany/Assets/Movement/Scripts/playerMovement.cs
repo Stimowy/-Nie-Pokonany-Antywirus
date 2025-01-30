de1a3cc902eb9837fcd3b7a6ac7a8c81b624fc8f
Sławomir Galary <slawomir.galary@gmail.com>
2025-01-30 15:55:10 +0100

    movement p3

--- NiePokonany/Assets/Movement/Scripts/playerMovement.cs
+++ NiePokonany/Assets/Movement/Scripts/playerMovement.cs
@@ -1,19 +1,40 @@
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class playerMovement : MonoBehaviour
 {
     [SerializeField] float movementSpeed;
     [SerializeField] Transform cameraRotation;
+    [SerializeField] Transform rotationDirection;
+    [SerializeField] Transform characterModel;
+    [SerializeField] Transform direction;
     private void FixedUpdate()
     {
         transform.position += transform.right * Input.GetAxis("Vertical") / movementSpeed * -1;
         transform.position += transform.forward * Input.GetAxis("Horizontal") / movementSpeed;
+        
+        float directionRotation;
         if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
-            Debug.Log(cameraRotation.transform.eulerAngles.y);
+            float frontBack = Input.GetAxis("Vertical");
+            float leftRight = Input.GetAxis("Horizontal");
+            int rot = 0;
+            if(leftRight < 0)
+            {
+                rot = 180;
+            }
+            //---
+            directionRotation = Mathf.Atan(frontBack/leftRight)*-180/Mathf.PI+90+rot;
 
             transform.eulerAngles = new Vector3(0, cameraRotation.eulerAngles.y, 0);
             cameraRotation.localEulerAngles = new Vector3(cameraRotation.eulerAngles.x,0, cameraRotation.eulerAngles.z);
+
+            rotationDirection.localEulerAngles = new Vector3(rotationDirection.localEulerAngles.x, cameraRotation.localEulerAngles.y+directionRotation, rotationDirection.localEulerAngles.z);
+            Vector3 dire = direction.position - transform.position;
+            quaternion rotation = Quaternion.LookRotation(dire)*Quaternion.Euler(90f*Vector3.up);
+            characterModel.rotation = Quaternion.Lerp(characterModel.rotation, rotation, 5f*Time.deltaTime);
         }
     }
 }
+//cameraRotation.localEulerAngles.y + (frontBack * 180)+(leftRight*90*frontBack2)
